a22a45a6b93742cea095ab428a092252dd90b32b
vrpvvaa <148848308+vrpvvaa@users.noreply.github.com>
2024-01-19 11:14:51 +0500

    ну все
    что то

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Info</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <div class="leftnav">
+            <h1>Тренировочные задания по ЕГЭ<br>
+                <span>ИНФОРМАТИКА</span><br>
+                <p>Уфимский государственный университет</p></h1>
+        </div>
+        <div class="navright">
+            <h1>Вы вошли на сайт как гость</h1>
+            <button>Войти</button>
+            <button>Зарегестрироваться</button>
+        </div>
+    </nav>
+    <main>
+        <h3>Выберите вариант заданий</h3>
+        <a href="/task.php?task=0">1 вариант</a>
+        <a href="/task.php?task=1">2 вариант</a>
+        <a href="/task.php?task=2">3 вариант</a>
+    </main>
+
+</html>
